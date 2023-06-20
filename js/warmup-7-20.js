d2386f6dfccaaca769397fd329ce71301ccbaf7b
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-06-20 11:04:04 -0500

    warm up for 7-20

---
+++ js/warmup-7-20.js
@@ -0,0 +1,19 @@
+function returnUserObject(username, password, email) {
+    return {
+        username: 'jreich',
+        password: 'password123',
+        email: 'jreich@email.com'
+    }
+}
+
+console.log(returnUserObject('jreich', 'password123', 'jreich@email.com'));
+
+// let email = [
+//     {
+//         username: 'jreich',
+//         password: 'password123',
+//         email: 'jreich@email.com'
+//     }
+// ]
+//
+// console.log(email)
