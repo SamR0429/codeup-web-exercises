ad9ab4dcbfa3b24daf02056719660e3ed21eafe9
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-06-26 14:22:27 -0500

    added and edited files

--- js/warmup-7-20.js
+++ js/warmup-7-20.js
@@ -22,16 +22,61 @@
 //june.21
 // Create a function, returnProduct, that takes in an array of numbers and returns the product of all the numbers.
 //     Assume all element in the input array are valid numbers. Assume all array inputs have at least 1 number.
-function returnProduct(numbers){
-   let total = 1;
-    for(let number of numbers){
-        total *= number;
-    }
-    return total;
-}
-console.log(returnProduct([1, 2, 3])); // returns 1 * 2 * 3 = 6
-console.log(returnProduct([3, 3, 3])); // returns 3 * 3 * 3 = 27
-console.log(returnProduct([4, 1, 2])); // returns 4 * 1 * 2 = 8
-console.log(returnProduct([4, 0, 2])); // returns 4 * 0 * 2 = 0
-console.log(returnProduct([5, 3, 2])); // returns 5 * 3 * 2 = 30
+// function returnProduct(numbers){
+//    let total = 1;
+//     for(let number of numbers){
+//         total *= number;
+//     }
+//     return total;
+// }
+// console.log(returnProduct([1, 2, 3])); // returns 1 * 2 * 3 = 6
+// console.log(returnProduct([3, 3, 3])); // returns 3 * 3 * 3 = 27
+// console.log(returnProduct([4, 1, 2])); // returns 4 * 1 * 2 = 8
+// console.log(returnProduct([4, 0, 2])); // returns 4 * 0 * 2 = 0
+// console.log(returnProduct([5, 3, 2])); // returns 5 * 3 * 2 = 30
 //you're saying up top that you have numbers which is the name we gave tha array, and were assuming that all array input are at least 1, that's the second line. then were saying that were trying to get to the individual numbers of that array so number is a singular value or numbers, that the second line. then were adding to the total every number that it runs into, then were returning the total at the end.
+
+//june.26
+// Create a function, returnTotalSales, that takes in an array of sales data objects and returns the sum of all
+// totalItemsSold properties. Assume all elements in the input array are valid sales data objects with a totalItemsSold
+// property.
+//
+//     EXAMPLE...
+
+const salesData = [
+    {
+        month: 'January',
+        totalItemsSold: 0
+    },
+    {
+        month: 'February',
+        totalItemsSold: 5
+    },
+    {
+        month: 'March',
+        totalItemsSold: 2
+    },
+    {
+        month: 'April',
+        totalItemsSold: 10
+    },
+    {
+        month: 'May',
+        totalItemsSold: 30
+    }
+];
+
+// function returnTotalSales(sales) {
+//     let returnTotalSales = 0
+//
+//     for (let sales of salesData) {
+//         returnTotalSales += sales.totalItemsSold;
+//     }
+// }
+// console.log(returnTotalSales(salesData));
+// YOU COULDNT DO IT CAUSE IT WAS INSIDE AN ARRAY !!!! PAY CLOSE ATTENTION
+
+const total = salesData[0].totalItemsSold + salesData[1].totalItemsSold /* etc etc  till finish */
+
+
+console.log(salesData[0].totalItemsSold);
