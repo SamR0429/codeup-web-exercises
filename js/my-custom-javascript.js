4569501b5a0e8c406a3a2f9d9df116386eebdff0
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-07-10 10:57:39 -0500

    added files

---
+++ js/my-custom-javascript.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// $(function() {
+//
+//     // INSERT JAVASCRIPT CODE SAMPLE HERE
+//
+// });
+
+$(function() {
+    alert( 'The DOM has finished loading!' );
+});
+window.onload = function() {
+    alert( 'The page has finished loading!' );
+}
