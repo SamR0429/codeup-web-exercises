b9b71ee005ffc7addab7b8a6335f5ddb7348a206
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-07-18 20:05:01 -0500

    redid the project

--- js/weather_map.js
+++ js/weather_map.js
@@ -7,7 +7,7 @@ $(() => {
     const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
     const map = initializeMap();
     const div = document.querySelector('#insertWeather');
-    // const searchBox =document.querySelector('#search-bar');
+    const searchBox = document.querySelector('#search-bar');
 
 
 ///////////////////////////////////////////////////////////////////////////////////
@@ -19,6 +19,25 @@ $(() => {
     }
 
     //function to activate the map
+    // function initializeMap() {
+    //     mapboxgl.accessToken = MAPBOX_TOKEN;
+    //
+    //     const mapOptions = {
+    //         container: 'map',
+    //         style: 'mapbox://styles/mapbox/navigation-night-v1',
+    //         zoom: 10,
+    //         center: [-98.4926, 29.4252],
+    //     }
+    //     const marker = new mapboxgl.Marker({
+    //         color: 'red',
+    //         draggable: true
+    //     })
+    //         .setLngLat([-98.4926, 29.4252])
+    //         .addTo(map);
+    //
+    //     return new mapboxgl.Map(mapOptions);
+    // }
+
     function initializeMap() {
         mapboxgl.accessToken = MAPBOX_TOKEN;
 
@@ -27,100 +46,90 @@ $(() => {
             style: 'mapbox://styles/mapbox/navigation-night-v1',
             zoom: 10,
             center: [-98.4926, 29.4252],
-        }
-        return new mapboxgl.Map(mapOptions);
-    }
+        };
 
-    //creation of draggable marker
-    let draggableMarker = new mapboxgl.Marker({
-        "color": 'red',
-        draggable: true
-    })
-        .setLngLat([-98.4926, 29.4252])
-        .addTo(map);
+        const map = new mapboxgl.Map(mapOptions); // Create the map instance
 
-    //function to render weather
-    function renderAllWeathers(list) {
+        const marker = new mapboxgl.Marker({
+            color: 'red',
+            draggable: true
+        })
+            .setLngLat([-98.4926, 29.4252])
+            .addTo(map);
 
-        const createdCard = document.createElement('ul');
-        // createdCard.innerHTML= '';
-        div.classList.add('sam-card');
-        createdCard.innerHTML = (`
-                <li class="date">${list.dt_txt}</li>
-                 <br>
-                 <li class="min-temp">LOW: ${list.main.temp_min}&deg;F</li>
-                 <hr>
-                 <li class="max-temp">HIGH: ${list.main.temp_max}&deg;F</li>
-                 <hr>
-                 <li class="conditions">Conditions: ${list.weather[0].description}</li>
-                 <hr>
-                 <li class="humidity">Humidity: ${list.main.humidity} %</li>
-                 <hr>
-                 <li class="wind">Wind: ${list.wind.speed} MPH</li>
-                 <hr>
-                 <li class="pressure">Pressure: ${list.main.pressure} hPa</li>
-                 <br>
-            `)
-        div.appendChild(createdCard);
-        //take and replace not make new cards
-    }
-
-    //for the draggable marker location rendering
-    function onDragEnd() {
-        let coords = draggableMarker.getLngLat();
-        getWeatherData(coords.lat, coords.lng);
-    }
-
-    //function to get weather for each day
-    function getWeatherData(lng,lat) {
-
-        $.ajax(getWeatherURL(lng, lat))
-            .done((data) => {
-
-                data.list.forEach((day, index) => {
-                    if (index % 8 === 0) {
-                        console.log(day.main.humidity);
-                        console.log(day.main.temp_min);
-                        console.log(day.main.temp_max);
-                        renderAllWeathers(day);
-                    }
-                });
-
-                $.ajax(getWeatherURL(lng, lat))
-                    .done(data => {
-                        console.log(data);
-                        const minMaxTemps = returnMinMaxTemps(data);
-                        minMaxTemps.forEach(minMaxTemp => {
-                            console.log(minMaxTemp);
-                        });
-                    })
-                    .fail(console.error);
-            })
+        marker.on('dragend');
+        return map;
     }
 
 
-    //input of lat and long by user
-    // function getLatLng(lng, lat) {
-    //     const URL = getWeatherURL([lng, lat]);
-    //     $.ajax(URL)
-    //         .done(data => {
-    //             console.log(data);
-    //             renderAllWeathers(data);
-    //         })
-    //         .fail(console.error)
+
+const generateWeather = $.ajax(getWeatherURL(29.4252,-98.4926 )).done(data =>{
+    console.log(data)
+$('#insertWeather').html('');
+
+    for(let i=0; i < data.list.length; i += 8){
+        const weatherCard = $('<div></div>')
+        weatherCard.html(`
+    <div class="sam-card">
+                     <p class="date">${data.list[i].dt_txt.split(' ')[0]}</p>
+                     <br>
+                     <p class="min-temp">LOW: ${data.list[i].main.temp_min}&deg;F</p>
+                     <hr>
+                     <p class="max-temp">HIGH: ${data.list[i].main.temp_max}&deg;F</p>
+                     <hr>
+                     <p class="conditions">Conditions: ${data.list[i].weather[0].description}</p>
+                     <hr>
+                     <p class="humidity">Humidity: ${data.list[i].main.humidity} %</p>
+                     <hr>
+                     <p class="wind">Wind: ${data.list[i].wind.speed} MPH</p>
+                     <hr>
+                     <p class="pressure">Pressure: ${data.list[i].main.pressure} hPa</p>
+                     <br>
+                </div>
+               
+`)
+        console.log(weatherCard);
+        $('#insertWeather').append(weatherCard);
+    }
+})
+    //you cant use a for of loop
+
+    // //function to render weather
+    // function renderAllWeathers(list) {
+    //     // const createdCard = document.createElement('ul');
+    //     // div.classList.add('sam-card');
+    //     // code to stop duplicating cards
+    //     // const parentCard = document.getElementById('insertWeather');
+    //     // parentCard.innerHTML = '';
+    //
+    //     let html = `
+    //
+
+    //         `
+    //     return html;
+    //     // div.appendChild(createdCard);
+    //     //take and replace not make new cards
     // }
+    //
+    // //for the draggable marker location rendering
+    // function onDragEnd() {
+    //     let coords = draggableMarker.getLngLat();
+    //     getWeatherData(coords.lat, coords.lng);
+    // }
+    //
+
 ///////////////////////////////////////////////////////////////////////////////////
 // Events
 ///////////////////////////////////////////////////////////////////////////////////
-    let coords = draggableMarker.getLngLat();
-    getWeatherData(coords.lat, coords.lng);
-
-    // draggableMarker.on("dragend", function () {
-    //     let coords = draggableMarker.getLngLat();
-    //     getWeatherData(coords.lat, coords.lng);
-    // })
-
-    draggableMarker.on("dragend", onDragEnd);
+//     let coords = draggableMarker.getLngLat();
+//     getWeatherData(coords.lat, coords.lng);
+//
+//     draggableMarker.on("dragend", function () {
+//         let coords = draggableMarker.getLngLat();
+//         getWeatherData(coords.lat, coords.lng);
+//     })
+//
+//     draggableMarker.on("dragend", onDragEnd);
 
 ///////////////////////////////////////////////////////////////////////////////////
 // Run When App Loads
