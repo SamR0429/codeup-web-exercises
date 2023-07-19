a8d9fd61f7493a8e3c9bf1b86f65d384856051c1
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-07-19 16:38:19 -0500

    finished es6 file

--- js/es6.js
+++ js/es6.js
@@ -26,38 +26,39 @@ const users = [
     name: 'justin',
     email: 'justin@codeup.com',
     languages: ['html', 'css', 'javascript', 'php']
-  }
+  },
+  // {
+  //   name: 'sam',
+  //   email: 'sam@codeup.com',
+  //   languages: ['html', 'css', 'javascript']
+  // }
 ];
-
+console.log(users);
 // TODO: fill in your name and email and add some programming languages you know
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'sam';
+const email = 'sam@codeup.com';
+const languages = ['html', 'css', 'javascript'];
 
 // TODO: rewrite the object literal using object property shorthand
-users.push({
-  name: name,
-  email: email,
-  languages: languages
-});
+users.push({name, email,languages});
 
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
+users.forEach((user) => {
   return emails.push(user.email);
 });
-users.forEach(function(user) {
+users.forEach((user) => {
   return names.push(user.name);
 });
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
@@ -71,12 +72,21 @@ users.forEach(function(user) {
 });
 
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+// developers.forEach(function (developer) {
 
-  // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
-});
-list += '</ul>';
+  for(let developer of developers) {
+
+    // TODO: rewrite the assignment below to use template strings
+//   list += '<li>' + developer + '</li>';
+// });
+// list += '</ul>';
+    list += `this is ${developer}\n`;
+
+  }
+
+console.log(list);
+  //this takes it out of the console and puts the array in the html page
+  // document.write(list);
