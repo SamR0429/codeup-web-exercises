59dd2fbc6dbfdc579e026afc47a3e6f92bad7c46
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-07-21 15:14:16 -0500

    commiting lecture notes

---
+++ js/promises-excersies.js
@@ -0,0 +1,36 @@
+
+    const getEvents = (userName) => {
+        const url = `https://api.github.com/users/${userName}/events`
+        const options = {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `token ${keys.github}`
+            }
+        }
+        fetch(url, options)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+    }
+
+
+
+(() => {
+    getEvents('SamR0429').then(events => {
+        console.log(events);
+        const pushEvents = events.filter(event => {
+            return event.type === "PushEvent";
+        });
+        console.log(pushEvents[0].created_at);
+    })
+
+
+})();
+
+
+//Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made
+
+
+
--- js/promises.js
+++ js/promises.js
@@ -1,28 +1,47 @@
-
-const getPerson =(id = 1) =>{
+const getPerson = (id = 1) => {
     const url = `https://swapi.dev/api/people/${id}/`
     const options = {
         method: 'GET',
         headers: {
-            "content-type": "application/json"
+            "content-type": "application/json",
         }
     }
     return fetch(url, options)
-        .then((response)=> {
+        .then((response) => {
             return response.json();
         })
-        // .then ((data)=>{
-        //     // console.log(data);
-        //     return data;
-        //
-        // });
+        .catch(error =>{
+            console.log(error.message)
+        })
+    // .then ((data)=>{
+    //     // console.log(data);
+    //     return data;
+    //
+    // });
 
 }
 
-
-
-
-
+const getFilm = (url) => {
+    const filmurl = url;
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }
+    return fetch(filmurl, options)
+        .then((response) => {
+            return response.json();
+        })
+        //this line is for showing an error that pops up showing what went wrong so we can see what it was actually that did it
+        .catch(error => {
+            console.log(error.message);
+        });
+    //the bottom line is redundant after we put the return next to the fetch in order to not be stuck in return hell
+    // .then((film) => {
+    //     console.log(film);
+    // });
+}
 (() => { // IIFE (Immediately Invoked Function Expression)
     // const url = 'https://swapi.dev/api/people/1/'
     // const options = {
@@ -38,6 +57,27 @@ const getPerson =(id = 1) =>{
     //     .then ((data)=>{
     //         console.log(data);
     //     });
-getPerson(4);
-//not accessible here
+
+    getPerson(1).then((person) => {
+        console.log(person);
+        //     const url = person.films[0];
+        //     const options = {
+        //         method: "GET",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //         }
+        //     }
+        //     fetch(url, options)
+        //         .then((response)=>{
+        //             return response.json();
+        //         })
+        //         .then((film)=>{
+        //             console.log(film);
+        //         });
+        getFilm(person.films[0]).then(film => {
+            console.log(film);
+        })
+    });
+
+
 })();
