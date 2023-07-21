fdb9cd1badb728d688742d55048959ce342d6c05
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-07-21 09:50:06 -0500

    comleted code for map-filter

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -50,20 +50,53 @@ const users = [
 // const atLeast3Langs = users.filter(user =>{
 //     return parseFloat(user.languages) >= 3;
 // })
-const atLeast3Langs = users.filter(user => user.languages >= 3);
+// const atLeast3Langs = users.filter(user => user.languages >= 3);
+//
+// console.log(atLeast3Langs);
+// console.log(atLeast3Langs);
+//
+//
+//
+//
+// const totalYearsExperience = users.reduce((accumulator, user, index) =>{
+//     const yearsArray = [];
+//     let yearsOfExperience = user.yearsOfExperience;
+//     for(let i = 0; i < users.length; i++){
+//         yearsOfExperience++
+//     }
+//     return yearsArray;
+// });
+// console.log(totalYearsExperience);
 
-console.log(atLeast3Langs);
-console.log(atLeast3Langs);
 
+const threeLang = users.filter(user => user.languages.length >= 3);
+console.log(threeLang);
 
+const emailArray = users.map(user => user.email);
+console.log(emailArray);
 
+const yearsOfExperience = users.reduce((accumulator, user,index)=>{
+    accumulator += parseFloat(user.yearsOfExperience);
+    return accumulator;
+}, 0);
+console.log(yearsOfExperience / users.length);
 
-const totalYearsExperience = users.reduce((accumulator, user, index) =>{
-    const yearsArray = [];
-    let yearsOfExperience = user.yearsOfExperience;
-    for(let i = 0; i < users.length; i++){
-        yearsOfExperience++
+const longestEmail = users.reduce((accumulator , user , index)=>{
+    const emailLength = user.email.length;
+    const accumulatorLength = accumulator.length;
+    if(emailLength > accumulatorLength){
+        accumulator = user.email;
     }
-    return yearsArray;
-});
-console.log(totalYearsExperience);
+    return accumulator;
+}, '');
+console.log(longestEmail);
+
+const usersString = users.reduce((accumulator , user , index)=>{
+    if(index === (users.length - 1)){
+        accumulator += `and ${user.name}.`
+    }else {
+        accumulator += `${user.name}, `
+    }
+    return accumulator;
+}, '')
+console.log(usersString);
