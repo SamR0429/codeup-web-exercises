60170b3d76206e5abb454340982a2cce1779f8ea
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-07-29 12:47:35 -0500

    worked on dom-practice file

--- practice files/js/practice-dom.js
+++ practice files/js/practice-dom.js
@@ -1,21 +1,52 @@
-
 //////////////////////////////////const
-// const problem1P = document.querySelector('#problem-1-p')
-
-
-
-
+//2.
+const body = document.querySelector('body')
+const problem2zBtn = document.querySelector('#problem-2-btn')
+const problem2Input = document.querySelector('#problem-2-input')
+//5.
+const problem5P = document.querySelector('#problem-5-p')
+//6.
+const problem6PInput = document.querySelector('#problem-6-input')
+const problem6Span = document.querySelector('#problem-6-span')
+//8.
+const h4List = document.querySelectorAll('h4')
+//------------------------------------------------------------------------------------
 
 
 //////////////////////////////////functions
-
-
-
-
-
+//2.
+function problem2() {
+    body.style.backgroundColor = problem2Input.value;
+}
+//5.
+function problem5() {
+    problem5P.classList.toggle('problem-5-color')
+}
+//6.
+function problem6() {
+    problem6Span.innerText = problem6PInput.value
+}
+//8.
+function problem8 (){
+    for(let h4 of h4List){
+        h4.classList.toggle('problem-8-color')
+    }
+}
+//-------------------------------------------------------------------------------------
 
 
 
 //////////////////////////////////event listeners
+
+problem2zBtn.addEventListener('click', problem2);
+
+problem5P.addEventListener('dblclick', problem5)
+
+problem6PInput.addEventListener('keyup',problem6)
+
+for(let h4 of h4List){
+    h4.addEventListener('click', problem8)
+}
+//-------------------------------------------------------------------------------------
 
 
